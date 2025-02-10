0a4a09352aebade2a3f7be0c25210a947bf0268c
Rohit Waghchaure <rohitw1991@gmail.com>
2025-02-10 19:09:02 +0530

    fix: check_item_quality_inspection is not whitelisted

--- erpnext/controllers/stock_controller.py
+++ erpnext/controllers/stock_controller.py
@@ -1564,6 +1564,7 @@ def repost_required_for_queue(doc: StockController) -> bool:
 	return False
 
 
+@frappe.whitelist()
 def check_item_quality_inspection(doctype, items):
 	if isinstance(items, str):
 		items = json.loads(items)
