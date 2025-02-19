9b8623dd64b5fab284831a137541d30ba98e3911
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2025-02-19 15:50:53 +0530

    chore: resolved conflicts

--- erpnext/patches/v15_0/create_asset_depreciation_schedules_from_assets.py
+++ erpnext/patches/v15_0/create_asset_depreciation_schedules_from_assets.py
@@ -3,15 +3,12 @@ from frappe.utils import cstr
 
 
 def execute():
-<<<<<<< HEAD
 	frappe.reload_doc("assets", "doctype", "Asset Depreciation Schedule")
 	frappe.reload_doc("assets", "doctype", "Asset Finance Book")
 	frappe.reload_doc("assets", "doctype", "Asset")
 
 	assets = get_details_of_draft_or_submitted_depreciable_assets()
 
-=======
->>>>>>> 7324dcb7c8 (fix: patch for creating asset depreciation schedule records)
 	asset_finance_books_map = get_asset_finance_books_map()
 	asset_depreciation_schedules_map = get_asset_depreciation_schedules_map()
 
