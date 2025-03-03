bd48d391e4a9c6e78e0e9896da9ebf8bce64efc5
Mihir Kandoi <kandoimihir@gmail.com>
2025-03-03 16:40:19 +0530

    fix: syntax error

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -1051,8 +1051,8 @@ def get_batch_based_item_price(params, item_code) -> float:
 	if not item_price:
 		item_price = get_item_price(params, item_code, ignore_party=True, force_batch_no=True)
 
-	if item_price and item_price[0].uom == params.get("uom") and not params.get("items", [{}])[0].get("is_free_item", 0):
-		return item_price[0].price_list_rate
+	if item_price and item_price[0][2] == params.get("uom") and not params.get("items", [{}])[0].get("is_free_item", 0):
+		return item_price[0][1]
 
 	return 0.0
 
