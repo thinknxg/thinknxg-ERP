bde55d2a07d81bac86e9a1253e7a1b4043e54823
ljain112 <ljain112@gmail.com>
2025-04-01 18:51:10 +0530

    fix: resolved conflicts

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -444,25 +444,16 @@ class PaymentEntry(AccountsController):
 				self.party_name = frappe.db.get_value(self.party_type, self.party, "name")
 
 		if self.party:
-<<<<<<< HEAD
-			if not self.contact_person:
-				set_contact_details(
-					self, party=frappe._dict({"name": self.party}), party_type=self.party_type
-				)
-			else:
-				complete_contact_details(self)
-			if not self.party_balance:
-				self.party_balance = get_balance_on(
-					party_type=self.party_type, party=self.party, date=self.posting_date, company=self.company
-				)
-=======
 			if self.party_type == "Employee":
 				self.contact_person = None
 			elif not self.contact_person:
 				self.contact_person = get_default_contact(self.party_type, self.party)
 
 			complete_contact_details(self)
->>>>>>> 8c9d630ee4 (Merge pull request #46683 from Abdeali099/set-employee-contact-details)
+			if not self.party_balance:
+				self.party_balance = get_balance_on(
+					party_type=self.party_type, party=self.party, date=self.posting_date, company=self.company
+				)
 
 			if not self.party_account:
 				party_account = get_party_account(self.party_type, self.party, self.company)
