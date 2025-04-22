982a68b71a7215168ad8075e900582689229ea2b
Mihir Kandoi <kandoimihir@gmail.com>
2025-04-22 15:50:02 +0530

    fix: change get_url_to_form to get_link_to_form
    (cherry picked from commit 5d07beee61102c2b5ab171dabc62dae6400e56c8)

--- erpnext/manufacturing/doctype/production_plan/production_plan.py
+++ erpnext/manufacturing/doctype/production_plan/production_plan.py
@@ -915,8 +915,7 @@ class ProductionPlan(Document):
 
 		if material_request_list:
 			material_request_list = [
-				f"""<a href="{get_url_to_form("Material Request", m.name)}">{m.name}</a>"""
-				for m in material_request_list
+				get_link_to_form("Material Request", m.name) for m in material_request_list
 			]
 			msgprint(_("{0} created").format(comma_and(material_request_list)))
 		else:
